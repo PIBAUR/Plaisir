cf6560f98581593a1cc2278b42ac3b024878fe6d
WilsonDigitalarti <wilson@digitalarti.com>
2015-01-23 15:04:17 +0100

    add .ignore for .cfgc

--- src/launch_utils/src/__init__.py
+++ src/launch_utils/src/__init__.py
@@ -7,18 +7,18 @@ from scenario_msgs.msg._VideoPlayer import VideoPlayer
 
 
 def launchRosNode(nodeName, launchFile):
-    os.environ["PYTHONPATH"] = "/home/artlab/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/groovy/lib/python2.7/dist-packages"
+    os.environ["PYTHONPATH"] = "/home/serveur/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/groovy/lib/python2.7/dist-packages"
     os.environ["ROS_DISTRO"] = "groovy"
     os.environ["ROS_ETC_DIR"] = "/opt/ros/groovy/etc/ros"
-    os.environ["ROS_HOME"] = "/home/artlab/.ros"
+    os.environ["ROS_HOME"] = "/home/serveur/.ros"
     os.environ["ROS_HOSTNAME"] = "192.168.150.1"
     os.environ["ROS_IP"] = "192.168.150.1"
-    os.environ["ROS_LOG_DIR"] = "/home/artlab/.ros/log"
+    os.environ["ROS_LOG_DIR"] = "/home/serveur/.ros/log"
     os.environ["ROS_MASTER_URI"] = "http://192.168.150.1:11311"
-    os.environ["ROS_PACKAGE_PATH"] = "/home/artlab/catkin_ws/src:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
+    os.environ["ROS_PACKAGE_PATH"] = "/home/serveur/catkin_ws/src:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
     os.environ["ROS_ROOT"] = "/opt/ros/groovy/share/ros"
-    os.environ["ROS_TEST_RESULTS_DIR"] = "/home/artlab/catkin_ws/build/test_results"
-    os.environ["ROSLISP_PACKAGE_DIRECTORIES"] = "/home/artlab/catkin_ws/devel/share/common-lisp"
+    os.environ["ROS_TEST_RESULTS_DIR"] = "/home/serveur/catkin_ws/build/test_results"
+    os.environ["ROSLISP_PACKAGE_DIRECTORIES"] = "/home/serveur/catkin_ws/devel/share/common-lisp"
     
     command = "/opt/ros/groovy/bin/roslaunch gui_controller " + launchFile
     sys.argv = ["roslaunch", nodeName, launchFile]
