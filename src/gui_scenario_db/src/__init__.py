b27ee8b7776e005e38b805207d15576905ee1f0e
ogilquin <ogilquin@gmail.com>
2015-02-11 13:03:26 +0100

    make every packages working with full eclipse launch / debug with ros

--- src/gui_scenario_db/src/__init__.py
+++ src/gui_scenario_db/src/__init__.py
@@ -26,13 +26,6 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 
-""" -----------------------------------
-    TO CHANGE DEPENDING ON THE PACKAGE:
-"""
-from ui import ScenarioDataBase
-NODE_CLASS = ScenarioDataBase
-""" ----------------------------------- """
-
 DEBUG_WITH_ROS = False
 
 def sigintHandler(*args):
@@ -58,7 +51,13 @@ if __name__ == '__main__':
             rospy.init_node(NODE_NAME, anonymous = True)
         
         app = QApplication(sys.argv)
-        main = NODE_CLASS()
+        
+        """ -----------------------------------
+        TO CHANGE DEPENDING ON THE PACKAGE:
+        """
+        from ui import ScenarioDataBase
+        main = ScenarioDataBase()
+        """ ----------------------------------- """
         
         sys.exit(app.exec_())
     except rospy.ROSInterruptException:
