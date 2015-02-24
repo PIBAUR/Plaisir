4b2cfc4c8778857c54a31f9b2325aa44176dbf74
ogilquin <ogilquin@gmail.com>
2015-02-24 17:44:51 +0100

    correct and improve obstacle_laserscan for issue #7

--- src/obstacle_laserscan/src/obstacle_laserscan.cpp
+++ src/obstacle_laserscan/src/obstacle_laserscan.cpp
@@ -2,71 +2,50 @@
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Bool.h>
 
-//*****************Global Variables********************
-sensor_msgs::LaserScan scan_data; 
-size_t node_count=210;
-size_t node_count2=180;
-//*****************headers**********************
-void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& laserScan){
+#define SCAN_RANGE 40
+#define MIN_LENGTH 0.2
+#define MAX_LENGTH 0.7
+#define MIN_SCAN_RANGE (180 - SCAN_RANGE / 2)
+#define MAX_SCAN_RANGE (180 + SCAN_RANGE / 2)
 
-scan_data.header.frame_id=scan_data.header.frame_id;
+std_msgs::Bool stop;
 
-        // values' copy
-	for(size_t i =150 ; i < node_count2; i++) // the angle goes from 135 degree to 180 degree (45 degrees to the right)
-		{       
-			scan_data.ranges[i] = laserScan->ranges[i];
-	 	 }
-	for(size_t i =180 ; i < node_count; i++) // the angle goes from 180 degree to 225 degrees (45 degrees to the left)
-		{       
-			scan_data.ranges[i] = laserScan->ranges[i];
-	 	 }	
+void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+	stop.data=false;
+
+	for(size_t i = MIN_SCAN_RANGE; i <= MAX_SCAN_RANGE; i++)
+	{
+		if((msg->ranges[i] < MAX_LENGTH) && (msg->ranges[i] > MIN_LENGTH)) //obstacle situated between 15cm and 30cm
+		{
+			ROS_INFO_STREAM("angle #" << i << " : " << msg->ranges[i]);
+			stop.data=true;
+			break;
+		}
+	}
  
 }
 
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "obstacle_laserscan_node");
+	ros::NodeHandle n;
+	//Subscriber
+	ros::Subscriber lidar_sub = n.subscribe<sensor_msgs::LaserScan>("scan", 1, lidarCallback);
+	//Publisher
+	ros::Publisher scan_pub = n.advertise<std_msgs::Bool>("front_obstacle", 1);
+	// frequency
+	ros::Rate r(5);
 
-//*************************************** 
-int main(int argc, char** argv){
+	stop.data=false;
 
-ros::init(argc, argv, "obstacle_laserscan_node");
-    
-ros::NodeHandle n;
+	//ROS Loop
+	while (ros::ok())
+	{
+		// message to be written
+		scan_pub.publish(stop);
+		ros::spinOnce();
+		r.sleep();
+	}
 
-//Subscriber
-ros::Subscriber lidar_sub = n.subscribe<sensor_msgs::LaserScan>("scan", 50, lidarCallback);
-//Publisher
-ros::Publisher scan_pub = n.advertise<std_msgs::Bool>("front_obstacle", 1);
-// frequency
-ros::Rate r(5);
-//ROS Loop
- 	while (ros::ok())
-		{
-			std_msgs::Bool stop;
-			stop.data=false;
-                        //***************************************************
-                        scan_data.ranges.resize(node_count); // Important to resize to the max value (360)
-                        //*****************************************************
-
-		for(size_t i =150 ; i < node_count2; i++)
-		{       
-			if((scan_data.ranges[i]<0.7)&&(scan_data.ranges[i]>0.15)) //obstacle situated between 15cm and 30cm
-				{       
-                      		 				stop.data=true;
-                        	}
-	 	 }
-		for(size_t i =180 ; i < node_count; i++) 
-		{       
-			if((scan_data.ranges[i]<0.7)&&(scan_data.ranges[i]>0.15))//obstacle situated between 15cm and 30cm
-				{       
-
-                         		 				stop.data=true;
-                        	}
-	 	 }
-          
-// message to be written 
-scan_pub.publish(stop);
-ros::spinOnce();
-r.sleep();
-//ros::spinOnce(); 
-		}
-return 0;
-   }
+ 	return 0;
+}
