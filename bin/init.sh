18a04a6c4b34157cb99caffc41558feae1b46585
WilsonDigitalarti <wilson@digitalarti.com>
2015-04-01 16:14:41 +0200

    add sh for setting time and run system

---
+++ bin/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+robot=01
+user=$USER
+
+#set time on odroid
+ssh odroid@192.168.150.1$robot 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+
+roslaunch robot init.launch
