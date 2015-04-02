bf1b7552c5f62bc2503979749510dc83dea403fd
WilsonDigitalarti <wilson@digitalarti.com>
2015-04-02 19:45:40 +0200

    Add init.sh for each robot. Update init.launch for multirobot May need update for robot00

--- bin/init.sh
+++ bin/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-robot=01
+robot=$1
+
 user=$USER
 
 #set time on odroid
 ssh odroid@192.168.150.1$robot 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
 
-roslaunch robot init.launch
+roslaunch robot init.launch robot:=$robot
