a736df6991614bf9330a1ca3537b9d687d441acd
artlab <artlab@artlab-serveur.(none)>
2015-04-20 12:12:15 +0200

    Add files for neato 2.

--- run/robot/init_1.sh
+++ run/robot/init_1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-robot=$1
-#robot=$2
 
-cd ~/catkin_make/run/mapping && make clean; make
+robot=$2
 
 user=$USER
 
 #set time on odroid
 ssh odroid@192.168.150.1$robot 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
 
-
-cd ~/catkin_ws/bin/ && ./mapping
+roslaunch robot init.launch robot:=$robot
