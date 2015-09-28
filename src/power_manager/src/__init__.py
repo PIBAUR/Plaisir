d4623b1b4918f5fbafe761a4c1da135972ff28e0
wilsondigitalarti <wilson@digitalarti.com>
2015-09-28 11:34:20 +0200

    Modif variables name. Slow down node rate to 0.2Hz (update every 5 seconds)

--- src/power_manager/src/__init__.py
+++ src/power_manager/src/__init__.py
@@ -10,18 +10,20 @@ NODE_NAME = "power_manager"
 import rospy
 from std_msgs.msg import Float64
 
+RATE = 0.2 # 0.2Hz --> Periode = 5 sec
+
 def execute():
     pub = rospy.Publisher('battery_percent', Float64)
     rospy.init_node('execute', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(RATE)
     while not rospy.is_shutdown():
         path = '/sys/devices/12d10000.adc/iio:device0/in_voltage0_raw'
         result = open(path, "r").read()
         result = result.replace("\r", "").replace("\n", "")
         result = float(result)/40.95 #conversion in percent
-        info_str = result % rospy.get_time()
-        rospy.loginfo(info_str)
-        pub.publish(info_str)
+        battery_percent = result % rospy.get_time()
+        rospy.loginfo(battery_percent)
+        pub.publish(battery_percent)
         rate.sleep()
  
 if __name__ == '__main__':
