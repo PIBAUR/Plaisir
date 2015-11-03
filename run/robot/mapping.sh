2734207590a49e9131fb2db58ced8ac4bcd90b43
wilsondigitalarti <wilson@digitalarti.com>
2015-11-03 11:28:06 +0100

    fix file path

--- run/robot/mapping.sh
+++ run/robot/mapping.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-robot=$1
 
-cd ~/catkin_ws/run/robot/mapping && make clean; make
+cd ~/catkin_ws/run/mapping && make clean; make
+
+ROBOT=$1
+
+
 
 user=$USER
 
 #set time on odroid
-ssh odroid@192.168.150.1$robot 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+ssh odroid@192.168.150.1$ROBOT 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
 
-cd ~/catkin_ws/bin/ && ./mapping $robot
+cd ~/catkin_ws/bin/ && ./mapping $ROBOT
