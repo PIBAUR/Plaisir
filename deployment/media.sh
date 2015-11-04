5f7a8cef0d800c76d5480bb652284bff0a1d8740
ogilquin <ogilquin@gmail.com>
2015-11-04 18:49:13 +0100

    remove --delete-after arg of the media deployment command, to not remove media not existing in src

--- deployment/media.sh
+++ deployment/media.sh
@@ -8,7 +8,7 @@ server_folder=`echo $server_folder| cut -d'"' -f 2`
 robot_folder=$(cat ~/catkin_ws/params/myparams.yaml | grep robot_videos_path)
 robot_folder=`echo $robot_folder| cut -d'"' -f 2`
 
-rsync -r -avz --delete-after \
+rsync -r -avz \
 	$server_folder/* odroid@192.168.150.1$robot:$robot_folder
 
 echo "deployment done on robot $robot"
