ebdf3596359f605fa9c4249ad4b3773629d453e7
ogilquin <ogilquin@gmail.com>
2015-11-10 18:05:43 +0100

    update multi robot launch

--- run/robot/launch.sh
+++ run/robot/launch.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-robot=$1
-
 user=$USER
 
 #set time on odroid
-ssh odroid@192.168.150.1$robot 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+echo "Sync time robot 00"
+ssh odroid@192.168.150.100 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+#ssh odroid@192.168.150.101 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+#ssh odroid@192.168.150.102 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+echo "Sync time robot 03"
+ssh odroid@192.168.150.103 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+#ssh odroid@192.168.150.104 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+#ssh odroid@192.168.150.105 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
+#ssh odroid@192.168.150.106 'echo odroid|sudo -S service ntp stop; echo odroid|sudo -S ntpdate 192.168.150.1'
 
 #launch robot
-roslaunch robot init.launch robot:=$robot
+roslaunch robot init_global.launch
