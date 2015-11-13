6b8bd81564ae7a524e4ca564af6f78701fba52b3
wilsondigitalarti <wilson@digitalarti.com>
2015-11-13 18:09:03 +0100

    another sh for linear speed test

---
+++ run/test/small_step_linear_speed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#TEST 1
+speed="1.0"
+step="0.01"
+
+echo "Test with speed_max = $speed  and step = $step"
+rosrun speed_test linear_speed _speed_max:=$speed _speed_step:=$step
+echo "Done."
+
+#TEST 2
+speed="0.15"
+step="0.05"
+
+echo "Test with speed_max = $speed  and step = $step"
+rosrun speed_test linear_speed _speed_max:=$speed _speed_step:=$step
+echo "Done."
+
+#TEST 1
+speed="0.10"
+step="0.001"
+
+echo "Test with speed_max = $speed  and step = $step"
+rosrun speed_test linear_speed _speed_max:=$speed _speed_step:=$step
+echo "Done."
+
