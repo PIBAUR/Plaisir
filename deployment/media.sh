9cb52fb97d482c86608ee7ea029a68fe43045cdc
ogilquin <ogilquin@gmail.com>
2015-11-18 15:31:49 +0100

    fix media deployment bug

--- deployment/media.sh
+++ deployment/media.sh
@@ -4,12 +4,13 @@ robot=$1
 
 server_folder=$(cat ~/catkin_ws/params/myparams.yaml | grep video_db_path)
 server_folder=`echo $server_folder| cut -d'"' -f 2`
+eval server_folder=$server_folder
 
 robot_folder=$(cat ~/catkin_ws/params/myparams.yaml | grep robot_videos_path)
 robot_folder=`echo $robot_folder| cut -d'"' -f 2`
+eval robot_folder=$robot_folder
 
-rsync -r -avz \
-	$server_folder/* odroid@192.168.1.1$robot:$robot_folder
+rsync -r -avz $server_folder/* odroid@192.168.1.1$robot:$robot_folder
 
 echo "deployment done on robot $robot"
 
