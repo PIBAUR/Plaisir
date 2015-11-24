1788e5899030a43828438bc320250bd47af6660b
ogilquin <ogilquin@gmail.com>
2015-11-24 10:35:33 +0100

    change params to test on robot03

--- run/robot/launch.sh
+++ run/robot/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 00
-bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 01
+#bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 01
 #bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 02
-#bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 03
+bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 03
 #bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 04
 #bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 05
 #bash ~/catkin_ws/run/robot/script_before_launch_for_robot.sh 06
