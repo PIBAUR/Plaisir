1ee2f0e75533cb87f8eb4a631d10ab0a5ec188cd
ogilquin <ogilquin@gmail.com>
2016-01-09 13:40:45 +0100

    Enhance random choice of scenario

--- src/scenario_lib/src/items/nodes/randomChoiceNode.py
+++ src/scenario_lib/src/items/nodes/randomChoiceNode.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+import math
 
 from PyQt4.QtGui import *
 
@@ -15,12 +16,14 @@ class RandomChoiceNode(DiagramNode):
     maxInputs = 50
     minInputs = 1
     hasOutput = 1
+    SIZE_RATIO = 0.50 #2/3
+    
     
     def __init__(self, robotId, parent, canvas, position):
         super(RandomChoiceNode, self).__init__(robotId, parent, canvas, position)
         
         self.randomIndex = -1
-        
+        self.lastInputsPlayed = []
     
     def output(self, args, updateRatioCallback):
         self.updateCallback = updateRatioCallback
@@ -28,7 +31,24 @@ class RandomChoiceNode(DiagramNode):
         inputs = self.getInputs()
         
         #if self.randomIndex == -1:
-        self.randomIndex = random.randint(0, len(inputs) - 1)
+        """
+        Random selector :
+            chose randomly the next scenario and check if it is one of the last X scenario played
+            X may be absolute or define according to the number of inputs
+        """
+        # update last played list
+        # "while" in case of list of inputs is shorter than previous iteration
+        self.lastInputsPlayed.append(self.randomIndex)
+        while len(self.lastInputsPlayed) > math.floor(len(inputs)*self.SIZE_RATIO):
+            self.lastInputsPlayed.pop(0)
+        # init new index
+        self.randomIndex = self.lastInputsPlayed[0]
+        # loop while new index is one of the last scenarios played
+        while self.randomIndex in self.lastInputsPlayed:
+            self.randomIndex = random.randint(0,len(inputs)-1)
+        """
+        End of modification
+        """
         
         inputItem = inputs[self.randomIndex]
         
